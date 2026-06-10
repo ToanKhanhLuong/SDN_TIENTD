0a208e8cf9152f79be22c71d36d933e57dc19bac
KhanhToan <toanlk290804@gmail.com>
2026-06-10 23:45:15 +0700

    ToanLk

--- Demo_SDN302_SUM26_1/Demo_Mongodb/index.js
+++ Demo_SDN302_SUM26_1/Demo_Mongodb/index.js
@@ -1,17 +1,24 @@
+
+
 // Đây là nơi code tạo ra server.
 const express = require("express");
 const data = require("./data"); // Giả sử data.js export mảng students
-
-
-
 
 // tạo server và trong đó sẽ chứa các API theo yêu cầu.
 const server = express();
 require("dotenv").config();
 server.use(express.json());
 
+// Khai Báo Đối Tượng Kết Nối DB
 
-
+const { MongoClient, ObjectId  } = require("mongodb");
+const dbClient = new MongoClient(process.env.DBURL);
+const connectDB = async () => {
+  await dbClient.connect();
+  console.log("Connected to MongoDB");
+  const dbName = await dbClient.db(process.env.DBNAME);
+  return dbName;
+};
 
 // Tạo các API
 // 1. API: GET / - trả về message chào mừng
@@ -22,189 +29,413 @@ server.get("/", (req, res) => {
 
 //-----------------------------------------------------------------------------------
 
-
-//2. API: GET /user - trả về danh sách user (hoặc students nếu data.js là students)
-server.get("/user", (req, res) => {
+//2. API: GET /departments - lấy danh sách departments
+server.get("/departments", async (req, res) => {
+  const db = await connectDB();
+  const collection = await db.collection("departments");
+  const data = await collection.find().toArray();
   res.status(200).json(data);
-})
+});
+
+//3. API: GET /employees - lấy danh sách employees :
+server.get("/employees", async (req, res) => {
+  const db = await connectDB();
+  const collection = await db.collection("employees");
+  const data = await collection.find().toArray();
+  res.status(200).json(data);
+});
+
+//4. API: GET /employees - lấy danh sách employees : id, name, salary, dob, gender, depnName
+
+server.get("/employees1", async (req, res) => {
+  const db = await connectDB();
+  const collection = db.collection("employees");
+
+  const data = await collection
+    .aggregate([
+      {
+        $lookup: {
+          from: "departments",
+          localField: "depId",
+          foreignField: "_id",
+          as: "depInfor",
+        },
+      },
+      {
+        $unwind: "$depInfor",
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          salary: 1,
+          gender: 1,
+          dob: 1,
+          depName: "$depInfor.depName",
+        },
+      },
+    ])
+    .toArray();
+
+  res.status(200).json(data);
+});
+
+// API: GET /employees1/:min/:max
+// Lấy danh sách nhân viên có salary nằm trong khoảng [min, max]
+// Hiển thị: id, name, salary, dob, gender, depName
+
+server.get("/employees1/:min/:max", async (req, res) => {
+  const min = Number(req.params.min);
+  const max = Number(req.params.max);
+
+  const db = await connectDB();
+
+  const data = await db
+    .collection("employees")
+    .aggregate([
+      {
+        $match: {
+          salary: {
+            $gte: min,
+            $lte: max,
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "departments",
+          localField: "depId",
+          foreignField: "_id",
+          as: "depInfor",
+        },
+      },
+      {
+        $unwind: "$depInfor",
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          salary: 1,
+          gender: 1,
+          dob: 1,
+          depName: "$depInfor.depName",
+        },
+      },
+    ])
+    .toArray();
+
+  res.status(200).json(data);
+});
 
 //-----------------------------------------------------------------------------------
 
-
-//3. API: POST /user - thêm mới user vào danh sách
-// tạo 1 middlaware check xem id đã tồn tại chưa
+//6. API: POST /department - thêm 1 department vào db
+// tạo 1 middlaware check xem name đã tồn tại chưa
 // nếu tồn tại thì báo lỗi k insert
 // Ngược lại insert bth
-const checkId = (req, res, next) =>{
-  let result = data.find((item) => item.id == req.body.id);
-  if(result){
-return next("Id da ton tai");
-  }else{
-    return next();
+const checkName = async (req, res, next) => {
+  const name = req.body.depName;
+
+  const db = await connectDB();
+  const collection = db.collection("departments");
+
+  const data = await collection.find({
+    depName: name
+  }).toArray();
+
+  if (data.length > 0) {
+    return next("Name da ton tai !!");
   }
-}
-server.post("/user", checkId,  (req, res) => {
-  let user = req.body;
-  data.push(user);
-  res.status(201).json({message : "Add successfully", user}); 
-});
 
-server.use((err, req, res, next) =>{
-  res.status(400).json({error: err})
-});
-
-
-
-//-----------------------------------------------------------------------------------
-
-
-
-
-// Luyện Tập
-//1. Update API GET danh sách user:
-     // - Hiển thị gender là male/ female thay cho 1/0
-     // - address chỉ hiển thị city.
-server.get("/users", (req, res) => {
-  const result = data.map(user => ({
-    ...user,
-    gender: user.gender === 1 ? "male" : "female",
-    address: user.address.city
-  }));
-
-  res.status(200).json(result);
-});
-
-
-//-----------------------------------------------------------------------------------
-
-
-
-//2. Update API POST insert user:  Thêm 1 middleware check input valid các trường
-const validateUserInput = (req, res, next) => {
-  const { name, age, gender, address } = req.body;
-
-  if (!name || name.trim() === "") {
-    res.status(400).json({message: "Name is required"});
-  } else if (age === undefined || isNaN(age) || age <= 0) {
-    res.status(400).json({message: "Age must be a positive number"});
-
-  } else if (gender !== 0 && gender !== 1) {
-    res.status(400).json({
-      message: "Gender must be 0 or 1"
-    });
-
-  } else if (!address || !address.city || address.city.trim() === "") {
-    res.status(400).json({
-      message: "Address city is required"
-    });
-
-  } else {
-    next();
-  }
+  next();
 };
+server.post("/departments", checkName, async(req, res) => {
+  let dep = req.body;
+   const db = await connectDB();
+  const collection = await db.collection("departments");
+   await collection.insertOne(dep);
+  res.status(201).json({ message: "Add successfully", DepInfor: dep });
+});
 
-server.post("/user", validateUserInput, (req, res) => {
-  const newUser = {
-    id: data.length + 1,
-    ...req.body
-  };
+server.use((err, req, res, next) => {
+  res.status(400).json({ error: err });
+});
 
-  data.push(newUser);
+//-----------------------------------------------------------------------------------
+//Practice:
+//1. Tạo API GET: /worksons/:hour -- > Tìm kiem danh sach worksons voi workhour>=hour,
+// hien thị: id, empid, empname, proid, proname, workhour
+//2. Tạo API POST:/employees -- > Insert employee vàodb,
+// có middleware check input kieu số cho salary, kieu date cho dob
+//3. Tạo API DELETE:/employees/:id -- >Delete employees theo id, có middleware check id ton tại
+//4. Tạo API PUT:/employees/:id -- >Update employees theo id, có middleware check id ton tại
+//5. Tạo API GET: /employees/: keyword -- > Search employee theo name dua vao keyword
+
+
+
+
+// GET /worksons/:hour
+// Tìm worksons có workHours >= hour
+// Hiển thị: id, empId, empName, proId, proName, workHours
+
+server.get("/worksons/:hour", async (req, res) => {
+  const hour = Number(req.params.hour);
+  const db = await connectDB();
+  const data = await db.collection("worksons").aggregate([
+    {
+      $match: {
+        workHours: {
+          $gte: hour
+        }
+      }
+    },
+    {
+      $lookup: {
+        from: "employees",
+        localField: "empId",
+        foreignField: "_id",
+        as: "empInfor"
+      }
+    },
+    {
+      $unwind: "$empInfor"
+    },
+    {
+      $lookup: {
+        from: "projects",
+        localField: "proId",
+        foreignField: "_id",
+        as: "proInfor"
+      }
+    },
+    {
+      $unwind: "$proInfor"
+    },
+    {
+      $project: {
+        _id: 1,
+        empId: 1,
+        empName: "$empInfor.name",
+        proId: 1,
+        proName: "$proInfor.proName",
+        workHours: 1
+      }
+    }
+  ]).toArray();
+
+  res.status(200).json(data);
+});
+
+
+
+//2. Tạo API POST:/employees -- > Insert employee vàodb,
+// có middleware check input kieu số cho salary, kieu date cho dob
+
+const checkEmployeeInput = (req, res, next) => {
+  const { salary, dob } = req.body;
+
+ if (isNaN(Number(salary))) {
+  return res.status(400).json({
+    error: "Salary phai la kieu so!"
+  });
+}
+
+  if (isNaN(Date.parse(dob))) {
+    return res.status(400).json({
+    error: "Dob phai la kieu date!"
+  });
+  }
+
+  next();
+};
+server.post("/employees", checkEmployeeInput, async (req, res) => {
+  const employee = req.body;
+
+  employee.salary = Number(employee.salary);
+
+  const db = await connectDB();
+  const collection = db.collection("employees");
+
+  const result = await collection.insertOne(employee);
 
   res.status(201).json({
-    message: "Insert user successfully", data: newUser
+    message: "Add employee successfully",
+    insertedId: result.insertedId,
+    employee: employee
   });
 });
 
 
 
-//-----------------------------------------------------------------------------------
+//3. Tạo API DELETE:/employees/:id -- >Delete employees theo id, có middleware check id ton tại
 
+// Bạn nhớ import ObjectId từ thư viện mongodb ở đầu file nhé
+ 
 
+const checkEmployeeId = async (req, res, next) => {
+  try {
+    const id = req.params.id.trim();
 
-//3. Tạo API GET: /user/:mark --> Tìm kiếm user theo mark
-server.get("/user/:mark", (req, res) => {
-  const mark = Number(req.params.mark);
-  const result = data.filter(user => user.grade === mark);
-  if (result.length === 0) {
-    return res.status(404).json({
-      message: "No user found"
-    });
+    // 1. Kiểm tra xem id truyền lên có đúng định dạng 24 ký tự hex của MongoDB không
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({
+        error: "Định dạng ID không hợp lệ!",
+      });
+    }
+
+    const db = await connectDB();
+    const collection = db.collection("employees");
+
+    // 2. Query trực tiếp dưới DB giống Mongo Shell
+    const employee = await collection.findOne({ _id: new ObjectId(id) });
+
+    if (!employee) {
+      return res.status(404).json({
+        error: "Employee không tồn tại!",
+      });
+    }
+
+    req.employee = employee;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  res.status(200).json(result);
-});
-
-server.get("/user")
-
-
-//-----------------------------------------------------------------------------------
-
-
-
-//4. Tạo API PUT: /user/:id ---> Update users theo id, có middleware check id tồn tại
-// Middleware check id tồn tại
-const checkUserId = (req, res, next) => {
-  const id = req.params.id;
-
-  const user = data.find(item => item.id === id);
-
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found"
-    });
-  }
-  next();
-};
-// PUT update user theo id
-server.put("/user/:id", checkUserId, (req, res) => {
-  const id = req.params.id;
-
-  const index = data.findIndex(item => item.id === id);
-
-  data[index] = {
-    ...data[index],
-    ...req.body
-  };
-  res.status(200).json({message: "Update successfully",data: data[index]
-  });
-});
-
-
-
-
-
-
-
-//5. Tạo API Delete; /user/:id --> Delete user theo id, có ,iddleware check id tồn tại.
-// Middleware check id tồn tại
-const checkUserIdDelete = (req, res, next) => {
-  const id = req.params.id;
-
-  const user = data.find(item => item.id === id);
-
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found"
-    });
-  }
-
-  next();
 };
 
-// DELETE user theo id
-server.delete("/user/:id", checkUserId, (req, res) => {
-  const id = req.params.id;
+server.delete("/employees/:id", checkEmployeeId, async (req, res, next) => {
+  try {
+    const db = await connectDB();
+    const collection = db.collection("employees");
 
-  const index = data.findIndex(item => item.id === id);
+    // Lệnh deleteOne này của bạn đã ổn rồi, req.employee._id hiện tại đang là 1 Object 
+    await collection.deleteOne({ _id: req.employee._id });
 
-  data.splice(index, 1);
-  res.status(200).json({message: "Delete successfully", data: data});
+    res.status(200).json({
+      message: "Delete employee successfully",
+      deletedEmployee: req.employee,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 
 
-// run server đã tạo ở bên trên để clien nào đó có thể call API.
+
+
+
+
+
+// Middleware check id ton tai
+// Middleware check id ton tai
+const checkEmployeeIdU = async (req, res, next) => {
+  try {
+    const id = req.params.id.trim();
+
+    // 1. Kiểm tra xem id truyền lên có đúng định dạng 24 ký tự hex của MongoDB không
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({
+        error: "Định dạng ID không hợp lệ!",
+      });
+    }
+
+    const db = await connectDB();
+    const collection = db.collection("employees");
+
+    // 2. Query trực tiếp dưới DB giống Mongo Shell
+    const employee = await collection.findOne({ _id: new ObjectId(id) });
+
+    if (!employee) {
+      return res.status(404).json({
+        error: "Employee không tồn tại!",
+      });
+    }
+
+    req.employee = employee;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+// PUT /employees/:id
+server.put("/employees/:id", checkEmployeeIdU, async (req, res, next) => {
+  try {
+    const db = await connectDB();
+    const collection = db.collection("employees");
+
+    const { name, salary, gender, dob, depId } = req.body;
+
+    // Xử lý depId an toàn hơn một chút
+    let formattedDepId = depId;
+    if (depId && ObjectId.isValid(depId)) {
+      formattedDepId = new ObjectId(depId);
+    }
+
+    // Dùng luôn req.employee._id từ middleware, không cần parse lại từ req.params.id
+    await collection.updateOne(
+      { _id: req.employee._id },
+      {
+        $set: {
+          name,
+          salary,
+          gender,
+          dob,
+          depId: formattedDepId,
+        },
+      }
+    );
+
+    // Lấy lại data sau khi update
+    const updatedEmployee = await collection.findOne({
+      _id: req.employee._id, 
+    });
+
+    res.status(200).json({
+      message: "Update employee successfully",
+      employee: updatedEmployee,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+//5. Tạo API GET: /employees/: keyword -- > Search employee theo name dua vao keyword
+
+
+server.get("/employees/:keyword", async (req, res, next) => {
+  try {
+    const keyword = req.params.keyword;
+
+    const db = await connectDB();
+    const collection = db.collection("employees");
+
+    const employees = await collection
+      .find({
+        name: { $regex: keyword, $options: "i" },
+      })
+      .toArray();
+
+    res.status(200).json({
+      message: "Tìm kiếm thành công!",
+      total: employees.length,
+      data: employees,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+
+
+
+
+// // run server đã tạo ở bên trên để clien nào đó có thể call API.
 const PORT = process.env.PORT || 8888;
 const HOST = process.env.HOST || "localhost";
 server.listen(PORT, HOST, () => {
